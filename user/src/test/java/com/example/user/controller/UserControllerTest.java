8c2cf143398810e1aa1a4335af63288c0ae034d7
Paulo <zhonions1234@gmail.com>
2024-02-07 19:31:46 +0000

    start intgration tests

---
+++ user/src/test/java/com/example/user/controller/UserControllerTest.java
@@ -0,0 +1,65 @@
+package com.example.user.controller;
+
+import com.example.user.entity.User;
+import com.example.user.repository.UserRepository;
+import com.example.user.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    public UserControllerTest() {
+    }
+
+    @Test
+    public void createUser() throws Exception {
+        User user = User.builder()
+                .name("name")
+                .password("Pass")
+                .build();
+
+        mockMvc.perform(post("/user")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(user)));
+    }
+
+    @Test
+    public void getAllUsers() {
+    }
+
+    @Test
+    public void findUserById() {
+    }
+
+    @Test
+    public void deleteUserById() {
+    }
+
+    @Test
+    public void updateUserById() {
+    }
+}
